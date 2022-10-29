710644af2b2cbc00fa7843a92b6a8ce6c340181c
zhitm <zhitnuhina.maria@gmail.com>
2022-10-29 18:26:40 +0300

    fixed savePhoto

--- VkApi/Program.cs
+++ VkApi/Program.cs
@@ -16,74 +16,73 @@ public class Response
             public string bdate { get; set; }
             public string music { get; set; }
             public string books { get; set; }
+            public string photo { get; set; }
+            public string server { get; set; }
+            public string hash { get; set; }
+            public string profile_aid { get; set; }
+            public string upload_url { get; set; }
+
         }
     }
  
+
+
     class VkApiClient
     {
-       private static async Task UsersGet()
+        private static string getHash(string response)
         {
-            var httpClient = new HttpClient();
- 
-            const string method = "users.get";
-            Console.WriteLine("Введите свой access token: ");
-            string accessToken = Console.ReadLine();
-            Console.WriteLine("Введите ID пользователя, о котором вы хотите получить информацию: ");
-            string userId = Console.ReadLine();
- 
-            var json = await httpClient.GetStringAsync(
-                $"https://api.vk.com/method/{method}?user_ids={userId}&fields=music&access_token={accessToken}&v=5.131"
-            );
- 
-            var userInfo = JsonConvert.DeserializeObject<Response>(json);
-            if (userInfo != null)
-            {
-                Console.WriteLine(userInfo.response[0].first_name + " " + userInfo.response[0].last_name);
-                Console.WriteLine(userInfo.response[0].music);
-            }
-            else
-            {
-                Console.WriteLine("Ошибка при чтении данных");
-            }
- 
-            Console.WriteLine("Нажмите <Enter>, чтобы закрыть консоль");
-            Console.ReadLine(); // задержка консоли. Актуально только для Windows
+            var info = JsonConvert.DeserializeObject<Response>(response);
+            return info.response[0].hash;
         }
- 
+        private static string getServer(string response)
+        {
+            var info = JsonConvert.DeserializeObject<Response>(response);
+            return info.response[0].server;
+        }
+        private static string getPhoto(string response)
+        {
+            var info = JsonConvert.DeserializeObject<Response>(response);
+            return info.response[0].photo;
+        }
+        private static string getUploadUrl(string response)
+        {
+            var info = JsonConvert.DeserializeObject<Response>(response);
+            // var str = info.response[0].upload_url;
+            return info.response[0].upload_url;
+        }
+        
         private static async Task VkAuthorize()
         {
+            var form = new MultipartFormDataContent
+            {
+                { new ByteArrayContent(File.ReadAllBytes("../../../photo.jpg"))
+                    , "photo", "photo.jpg" }
+            };  
             var client = new HttpClient();
-            const int clientId = 2685278;
+            const int clientId = 7499511;
             const string redirectUri = "https://oauth.vk.com/blank.html";
             Console.WriteLine("Введите ID пользователя, информацию о котором вы хотите получить (целое число): ");
-            var userId = int.Parse(Console.ReadLine()); // здесь userId нужен только в формате int
+            var userId = 343537675;
+            // var userId = int.Parse(Console.ReadLine()); // здесь userId нужен только в формате int
             var authString = $"https://oauth.vk.com/authorize?client_id={clientId}&display=page&redirect_uri={redirectUri}&scope=friends&response_type=token&v=5.131&state=123456";
             authString = authString.Replace("&", "^&");
             Process.Start(new ProcessStartInfo("cmd", $"/c start {authString}") { CreateNoWindow = true });
- 
             Console.WriteLine("Введите полученный access token: ");
-            var access_token = Console.ReadLine();
-            var json = await client.GetStringAsync($"https://api.vk.com/method/friends.getOnline?user_id={userId}&v=5.131&access_token={access_token}");
- 
-            var (startIndex, endIndex) = (json.IndexOf("["), json.IndexOf("]"));
- 
-            var friendsList = json.Substring(startIndex + 1, endIndex - startIndex - 1).Split(',').Select(prop => int.Parse(prop)).ToList();
-            foreach (var id in friendsList)
-            {
-                var userResponse = await client.GetStringAsync($"https://api.vk.com/method/users.get?user_id={id}&v=5.131&fields=bdate&access_token={access_token}");
-                var userAccount = JsonConvert.DeserializeObject<Response>(userResponse);
-                try
-                {
-                    Console.WriteLine(userAccount.response[0].first_name + " " + userAccount.response[0].last_name);
-                }
-                catch
-                {
-                    Console.WriteLine("Ошибка чтения данных");
-                }
-            }
- 
-            Console.WriteLine("Нажмите <Enter>, чтобы закрыть консоль");
-            Console.ReadLine(); // задержка консоли. Актуально только для Windows
+            string access_token = Console.ReadLine();
+            
+            //to get link
+            var json = await client.GetStringAsync($"https://api.vk.com/method/photos.getOwnerPhotoUploadServer?owner_id={userId}&v=5.131&access_token={access_token}");
+            var url = getUploadUrl(json);
+            var response = await client.PostAsync(url, form);
+
+            // var response = await client.PostAsync("https://pu.vk.com/c230131/ss2019/upload.php?_query=eyJhY3QiOiJvd25lcl9waG90byIsInNhdmUiOjEsImFwaV93cmFwIjp7InNlcnZlciI6OTk5LCJwaG90byI6IntyZXN1bHR9IiwibWlkIjozNDM1Mzc2NzUsImhhc2giOiI3NDQ4ZjkzZjQwNWRmZDVjNDQ0ZjVkNGFhYmI4NDk0MiIsIm1lc3NhZ2VfY29kZSI6MiwicHJvZmlsZV9haWQiOi02fSwib2lkIjozNDM1Mzc2NzUsIm1pZCI6MzQzNTM3Njc1LCJzZXJ2ZXIiOjIzMDEzMSwiX29yaWdpbiI6Imh0dHBzOlwvXC9hcGkudmsuY29tIiwiX3NpZyI6IjgzY2MyNjZkZjEyZDlhNGU1MmU4YTMxMDYyZWZkZjNhIn0", form);
+            var responseString = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(responseString);
+            var photo = getPhoto(responseString);
+            var server = getServer(responseString);
+            var hash = getHash(responseString);
+            var savePhoto = await client.GetStringAsync($"https://api.vk.com/method/photos.saveOwnerPhoto?hash={hash}&server={server}&photos={photo}&v=5.131&access_token={access_token}");
+            Console.WriteLine(savePhoto);
         }
         private static async Task Main()
         {
